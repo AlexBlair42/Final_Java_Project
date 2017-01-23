38fb644bb665e02c20665956186bdb2727280a40
Alex Blair <ablair18@my.whitworth.edu>
2017-01-23 14:49:50 -0800

    Big work done

--- Final_Project/src/Main.java
+++ Final_Project/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+public final class Main {
+	
+	private final Character character = Character.newInstance();
+	
+	public void play() throws IOException{
+		System.out.println("You are " + character + " " + character.getDescription());
+		Dungeon.newInstance().startAdventure(character);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main main = new Main();
+		main.play();
 
 	}
 
